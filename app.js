106a587cf70464e36e65d73c8afbda8384bd8cc6
shashankchandrakar <shashankchandrakarr@gmail.com>
2021-03-20 18:42:31 +0530

    logic changed

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
-
-express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
 
 const qrcode = require('qrcode-terminal');
 
@@ -63,4 +54,14 @@ client.on('authenticated', (session) => {
             console.error(err);
         }
     });
+    console.log("authenticated ",session);
 });
+
+const app = express();
+
+app.set('port',PORT)
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
+
